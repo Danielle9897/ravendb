4139f0761c08c0b90ac9ec38a6f51bda47cfeaec
Danielle9897 <danielle.9897@gmail.com>
2021-11-18 18:21:03 +0200

    RavenDB-17537 Error when querying Auto-Index with quotes

--- src/Raven.Studio/typescript/models/database/query/queryCriteria.ts
+++ src/Raven.Studio/typescript/models/database/query/queryCriteria.ts
@@ -75,13 +75,20 @@ class queryCriteria {
 
     setSelectedIndex(indexName: string): void {
         let rql = "from ";
-        if (indexName.startsWith(queryUtil.DynamicPrefix)) {
+        
+        if (indexName.toLocaleLowerCase().startsWith(queryUtil.AutoPrefix) && indexName.includes("'")) {
+            rql += `index "${indexName}"`;
+        
+        } else if (indexName.startsWith(queryUtil.DynamicPrefix)) {
             rql += indexName.substring(queryUtil.DynamicPrefix.length);
+        
         } else if (indexName === queryUtil.AllDocs) {
             rql += "@all_docs";
+        
         } else {
             rql += "index '" + indexName + "'";
         }
+
         this.queryText(rql);
     }
 
