bdf928d373c7f7145acac4c8ee63c559fd794497
Stav <stav@ravendb.net>
2022-03-28 11:51:19 +0300

    RavenDB-18201 - Sharding - Edit Document View Endpoints - time series stats

---
+++ src/Raven.Server/Documents/Sharding/Handlers/ShardedTimeSeriesHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Raven.Server.Documents.Sharding.Handlers.Processors.TimeSeries;
+using Raven.Server.Routing;
+
+namespace Raven.Server.Documents.Sharding.Handlers
+{
+    public class ShardedTimeSeriesHandler : ShardedDatabaseRequestHandler
+    {
+        [RavenShardedAction("/databases/*/timeseries/stats", "GET")]
+        public async Task Stats()
+        {
+            using (var processor = new ShardedTimeSeriesHandlerProcessorForGetTimeSeriesStats(this))
+            {
+                await processor.ExecuteAsync();
+            }
+        }
+    }
+}
