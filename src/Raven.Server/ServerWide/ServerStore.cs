4950feed60de814caeafa964989128c2d45a7bbc
Omer Michlewitz <omer@ravendb.net>
2022-05-10 13:59:18 +0300

    RavenDB-18492 - [WIP] Unsecured SetupWizard and RVN flows implementation

--- src/Raven.Server/ServerWide/ServerStore.cs
+++ src/Raven.Server/ServerWide/ServerStore.cs
@@ -82,6 +82,7 @@ using Sparrow.Utils;
 using Voron;
 using Voron.Exceptions;
 using Constants = Raven.Client.Constants;
+using NodeInfo = Raven.Client.ServerWide.Commands.NodeInfo;
 
 namespace Raven.Server.ServerWide
 {
