abd637155551f6d7f1ceda4fe9ccc2dca06c60ea
Arkadiusz Palinski <arek@ravendb.net>
2025-02-14 13:40:24 +0100

    RavenDB-23556 More "AI ETL" to "AI Integration" renames. Moving tests and namespace adjustments.

--- src/Raven.Server/ServerWide/ServerStore.cs
+++ src/Raven.Server/ServerWide/ServerStore.cs
@@ -2657,7 +2657,7 @@ namespace Raven.Server.ServerWide
 
                         var aiEtls = rawRecord.AiIntegrations;
 
-                        // Don't delete the connection string if used by tasks types: AI ETL
+                        // Don't delete the connection string if used by tasks types: AI Integration
                         if (aiEtls != null)
                         {
                             foreach (var aiEtlTask in aiEtls)
