e1be9b889c911eae49272e18d6e4b4951a282a85
Maciej Aszyk <maciej@ravendb.net>
2025-02-20 15:41:27 +0100

    RavenDB-23556 Removing AiTask from VectorOptions

--- src/Raven.Server/Json/BlittableJsonTextWriterExtensions.cs
+++ src/Raven.Server/Json/BlittableJsonTextWriterExtensions.cs
@@ -1788,14 +1788,6 @@ namespace Raven.Server.Json
                     writer.WriteString(options.Vector.NumberOfCandidatesForIndexing.ToString());
                 else
                     writer.WriteNull();
-
-                writer.WriteComma();
-                
-                writer.WritePropertyName(nameof(options.Vector.AiIntegrationIdentifier));
-                if (options.Vector.AiIntegrationIdentifier is not null)
-                    writer.WriteString(options.Vector.AiIntegrationIdentifier);
-                else
-                    writer.WriteNull();
                 
                 writer.WriteEndObject();
             }
