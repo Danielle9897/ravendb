e022a1e24b21728e9316d67b763b4443b9139a9a
Pawel Pekrol <ppekrol@ravendb.net>
2026-04-03 23:28:45 +0200

    RavenDB-24310 Address code review findings
    - Fix dead Modifications assignment in PutServerWideConnectionStringCommand.GetUpdatedValue
      that caused connection string updates to write to a discarded local variable
    - Add excludedDatabasesPropertyName parameter to IsExcluded to decouple from
      IServerWideTask; callers pass nameof(IServerWideTask.ExcludedDatabases) or
      nameof(ServerWideConnectionString.ExcludedDatabases) as appropriate
    - Move csJson creation inside the per-database loop to avoid reusing a mutable
      DynamicJsonValue across iterations
    - Make ServerWideConfigurationKey connection string fields static readonly
    - Rename DeleteServerWideConnectionStringOperation.cs to match class name
      RemoveServerWideConnectionStringOperation
    - Add license check (AssertServerWideFor) on RemoveServerWideConnectionStringCommand
    - Remove GetDatabaseRecordConnectionStringName wrapper from command; callers now
      use ServerWideConnectionString.GetDatabaseRecordConnectionStringName directly

--- src/Raven.Server/ServerWide/ClusterStateMachine.cs
+++ src/Raven.Server/ServerWide/ClusterStateMachine.cs
@@ -652,6 +652,7 @@ namespace Raven.Server.ServerWide
                         break;
 
                     case nameof(RemoveServerWideConnectionStringCommand):
+                        AssertServerWideFor(serverStore, LicenseAttribute.ServerWideConnectionStrings);
                         var deleteCSConfiguration = UpdateValue<RemoveServerWideConnectionStringCommand.DeleteConfiguration>(context, type, cmd, index, skipNotifyValueChanged: true);
                         RemoveServerWideConnectionStringFromAllDatabases(deleteCSConfiguration, context, type, index);
                         break;
@@ -2059,7 +2060,7 @@ namespace Raven.Server.ServerWide
                     if (serverWideBackups.TryGet(propertyName, out BlittableJsonReaderObject configurationBlittable) == false)
                         continue;
 
-                    if (IsExcluded(configurationBlittable, addDatabaseCommand.Name))
+                    if (IsExcluded(configurationBlittable, addDatabaseCommand.Name, nameof(IServerWideTask.ExcludedDatabases)))
                         continue;
 
                     configurationBlittable.TryGet(nameof(ServerWideBackupConfiguration.BackupType), out BackupType backupType);
@@ -2123,7 +2124,7 @@ namespace Raven.Server.ServerWide
                     if (configurationBlittable.TryGet(nameof(ServerWideExternalReplication.TopologyDiscoveryUrls), out BlittableJsonReaderArray topologyDiscoveryUrlsBlittableArray) == false)
                         continue;
 
-                    if (IsExcluded(configurationBlittable, addDatabaseCommand.Name))
+                    if (IsExcluded(configurationBlittable, addDatabaseCommand.Name, nameof(IServerWideTask.ExcludedDatabases)))
                         continue;
 
                     var topologyDiscoveryUrls = topologyDiscoveryUrlsBlittableArray.Select(x => x.ToString()).ToArray();
@@ -2161,9 +2162,9 @@ namespace Raven.Server.ServerWide
             };
         }
 
-        private static bool IsExcluded(BlittableJsonReaderObject configurationBlittable, string databaseName)
+        private static bool IsExcluded(BlittableJsonReaderObject configurationBlittable, string databaseName, string excludedDatabasesPropertyName)
         {
-            if (configurationBlittable.TryGet(nameof(IServerWideTask.ExcludedDatabases), out BlittableJsonReaderArray excludedDatabases) == false || excludedDatabases == null)
+            if (configurationBlittable.TryGet(excludedDatabasesPropertyName, out BlittableJsonReaderArray excludedDatabases) == false || excludedDatabases == null)
                 return false;
 
             foreach (object excludedDatabase in excludedDatabases)
