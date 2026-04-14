1ff62245a47f93425c7b96dedd250c9f4c523556
Maksym Smoliński <maksym.smolinski@ravendb.net>
2026-04-14 12:54:58 +0200

    RavenDB-24310 feat(connection strings): add server-wide connection strings handling and UI adjustments in ConnectionStringsPanel
    RavenDB-24310 feat(connection strings): enhance server-wide connection strings with fetch actions and UI improvements

--- src/Raven.Studio/typescript/components/pages/database/settings/connectionStrings/store/connectionStringsSlice.ts
+++ src/Raven.Studio/typescript/components/pages/database/settings/connectionStrings/store/connectionStringsSlice.ts
@@ -15,12 +15,18 @@ import {
     mapSnowflakeConnectionsFromDto,
     mapAmazonSqsConnectionsFromDto,
     mapAiConnectionsFromDto,
+    mapServerWideConnectionsFromDto,
+    ServerWideConnectionStringDto,
 } from "./connectionStringsMapsFromDto";
 import { accessManagerSelectors } from "components/common/shell/accessManagerSliceSelectors";
 import DatabaseUtils from "components/utils/DatabaseUtils";
 import { databaseSelectors } from "components/common/shell/databaseSliceSelectors";
 
-export type ConnectionStringsViewContext = "connectionStrings" | "aiConnectionStrings" | "aiTask";
+export type ConnectionStringsViewContext =
+    | "connectionStrings"
+    | "aiConnectionStrings"
+    | "aiTask"
+    | "serverWideConnectionStrings";
 
 interface ConnectionStringsState {
     loadStatus: loadStatus;
@@ -149,6 +155,16 @@ export const connectionStringsSlice = createSlice({
             })
             .addCase(fetchData.rejected, (state) => {
                 state.loadStatus = "failure";
+            })
+            .addCase(fetchServerWideData.fulfilled, (state, { payload }) => {
+                state.connections = mapServerWideConnectionsFromDto(payload.serverWideDto);
+                state.loadStatus = "success";
+            })
+            .addCase(fetchServerWideData.pending, (state) => {
+                state.loadStatus = "loading";
+            })
+            .addCase(fetchServerWideData.rejected, (state) => {
+                state.loadStatus = "failure";
             });
     },
 });
@@ -157,6 +173,10 @@ interface FetchDataResult {
     ongoingTasksDto: Raven.Server.Web.System.OngoingTasksResult;
     connectionStringsDto: GetConnectionStringsResult;
     hasDatabaseAdminAccess: boolean;
+}
+
+interface FetchServerWideDataResult {
+    serverWideDto: ServerWideConnectionStringDto[];
 }
 
 const fetchData = createAsyncThunk<
@@ -185,9 +205,18 @@ const fetchData = createAsyncThunk<
     };
 });
 
+const fetchServerWideData = createAsyncThunk<FetchServerWideDataResult>(
+    connectionStringsSlice.name + "/fetchServerWideConnectionStrings",
+    async () => {
+        const { Results } = await services.tasksService.getServerWideConnectionStrings();
+        return { serverWideDto: Results };
+    }
+);
+
 export const connectionStringsActions = {
     ...connectionStringsSlice.actions,
     fetchData,
+    fetchServerWideData,
 };
 
 export const connectionStringSelectors = {
